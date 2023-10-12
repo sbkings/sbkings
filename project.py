ffeb2ba4c7a42396fd5f327c3e4b2b0e8af5fec8
sbkings <147301871+sbkings@users.noreply.github.com>
2023-10-12 16:57:14 +0530

    Add files via upload

--- project.py
+++ project.py
@@ -109,13 +109,40 @@ def season():
          print('the rf average in the premonsoon season of the',year,':',pre/c1,'the rf average in the southwestmonsoon season of the',year,':',swm/c2,'the rf  the northeastmonsoon ',year,':',nem/c3)
       except:
             print('bye')
+def rainyday():
+    import csv
+    city=input('enter the city')
+    a=city+'.csv'
+    f=open(a,'r',newline='')
+    v=csv.reader(f)
+    year=int(input('eneter the year'))
+    month=int(input('enter the month'))
+    k=0
+    count=0
+    for i in v:
+      for i in v:
+       if k==0:
+         k+=1
+         continue
+       else:
+        b=i[0].split('-')
+        if year==int(b[-1]) and int(b[-2])==month and i[-1]!=''and int(float(i[-1]))>0:
+              count+=1
+        
+        else:
+             continue
+    print(count)
+    
 ans='yes'       
 while ans=='yes':
-    print('welcome to ssr weather forecasitng system(ssrfs)')
+    print('welcome to ssr weather analysis unit')
+    print('Available cities,1)Chennai,2)Mumbai,3)Banglore,4)Delhi')
     print('available cities:')
     print("1)To find a year's average enter 1")
     print('2)to find the avg of a month in a partiuclar year:')
     print('3)to check the data on a paticular date')
+    print('4)to know the season average of a particular year enter option 4')
+    print('5)to know the number of rainy days in a month of a particular year')
     opt=int(input('enter the option that you you want to venture:'))
     if opt==1:
        yearavg()
@@ -125,6 +152,8 @@ while ans=='yes':
       date()
     if opt==4:
       season()
+    if opt==5:
+        rainyday()
     ans=input('do you wish to analyse more:')
 
     
